5dbc9dd91b1bd87dda6d393ad198822b680516a0
VaishnaviM411 <vaishnaviv@sahaj.ai>
2023-03-21 16:05:02 +0530

    Mock fetch function in tests

--- __tests__/apiService.test.js
+++ __tests__/apiService.test.js
@@ -1,43 +1,92 @@
 import CreateOrderAPIService from "../src/script/apiService.js"
 
 
+
 describe("Create Order Tests", () => {
     let createOrderService = new CreateOrderAPIService()
 
     it("should return error given insufficient amount in wallet", async () => {
-        let response = await createOrderService.placeOrderAPI(
-            JSON.stringify({
-                "type": "BUY",
-                "quantity": 10,
-                "price": 10
-            })
-            , "harsh")
+        const mockRequestBody = JSON.stringify({
+            "type": "BUY",
+            "quantity": 10,
+            "price": 10
+        })
+        const body = mockRequestBody
+        const mockUsername = "harsh"
+        global.fetch = jest.fn().mockImplementationOnce(() => Promise.resolve({
+            json: () => Promise.resolve({ "error": ["insufficient wallet funds"] })
+        }));
 
+
+        let response = await createOrderService.placeOrderAPI(mockRequestBody, mockUsername)
+
+        expect(fetch).toHaveBeenCalledTimes(1)
+        expect(fetch).toHaveBeenCalledWith(`http://localhost:8080/user/${mockUsername}/order`, { body, "headers": { "Content-Type": "application/json" }, "method": "POST" })
         expect(response).toEqual({ "error": ["insufficient wallet funds"] })
+
+        global.fetch.mockRestore()
     })
 
     it("should return error if user does not exist", async () => {
-        let response = await createOrderService.placeOrderAPI(
-            JSON.stringify({
-                "type": "BUY",
-                "quantity": 10,
-                "price": 10
-            })
-            , "john")
+        const mockRequestBody = JSON.stringify({
+            "type": "BUY",
+            "quantity": 10,
+            "price": 10
+        })
+        const body = mockRequestBody
+        const mockUsername = "john"
+        global.fetch = jest.fn().mockImplementationOnce(() => Promise.resolve({
+            json: () => Promise.resolve({ "error": ["user does not exists"] })
+        }))
 
+        let response = await createOrderService.placeOrderAPI(mockRequestBody, mockUsername)
+
+        expect(fetch).toHaveBeenCalledTimes(1)
+        expect(fetch).toHaveBeenCalledWith(`http://localhost:8080/user/${mockUsername}/order`, { body, "headers": { "Content-Type": "application/json" }, "method": "POST" })
         expect(response).toEqual({ "error": ["user does not exists"] })
+
+        global.fetch.mockRestore()
     })
 
     it("should place order", async () => {
-        let response = await createOrderService.placeOrderAPI(
-            JSON.stringify({
+        const mockRequestBody = JSON.stringify({
+            "type": "BUY",
+            "quantity": 10,
+            "price": 10
+        })
+        const body = mockRequestBody
+        const mockUsername = "vaishnavi"
+        global.fetch = jest.fn().mockImplementationOnce(() => Promise.resolve({
+            json: () => Promise.resolve({
+                "orderId": "16",
+                "username": "vaishnavi",
                 "type": "BUY",
-                "quantity": 10,
-                "price": 10
+                "quantity": 20,
+                "price": 10,
+                "esopType": "NON_PERFORMANCE",
+                "status": "PLACED",
+                "remainingQuantity": 20,
+                "createdAt": 1679394821574
             })
-            , "vaishnavi")
+        }))
 
-        expect(response.status).toEqual("PLACED")
+        let response = await createOrderService.placeOrderAPI(mockRequestBody, mockUsername)
+
+        expect(fetch).toHaveBeenCalledTimes(1)
+        expect(fetch).toHaveBeenCalledWith(`http://localhost:8080/user/${mockUsername}/order`, { body, "headers": { "Content-Type": "application/json" }, "method": "POST" })
+        expect(response).toEqual({
+            "orderId": "16",
+            "username": "vaishnavi",
+            "type": "BUY",
+            "quantity": 20,
+            "price": 10,
+            "esopType": "NON_PERFORMANCE",
+            "status": "PLACED",
+            "remainingQuantity": 20,
+            "createdAt": 1679394821574
+        })
+
+        global.fetch.mockRestore()
     })
 
 })
